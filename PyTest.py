24552ae10f2021da4218c525a41a660e1058f1d2
Tefileo Stephens <TStephens@spartaglobal.com>
2020-03-02 15:49:02 +0000

    Tests Updated

--- PyTest.py
+++ PyTest.py
@@ -4,72 +4,88 @@ import sys
 reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
+
+
+def Fail_Program():
+    assert ("fail_program" in installed_packages)
+
 try:
     assert ("atomicwrites" in installed_packages)
-    print("atomicwrites installation test passed..... overall progress 10%")
+    print("atomicwrites installation test passed..... overall progress 10%\n\n")
 
 except AssertionError:
-    print ('atomicwrites installation test Failed')
+    print ('atomicwrites installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("attrs" in installed_packages)
-    print("attrs installation test passed..... overall progress 20%")
+    print("attrs installation test passed..... overall progress 20%\n\n")
 
 except AssertionError:
-    print ('attrs installation test Failed')
+    print ('attrs installation test Failed\n\n')
+    Fail_Program()
 
 try:
 
     assert ("beautifulsoup4" in installed_packages)
-    print("beautifulsoup4 installation test passed..... overall progress 30%")
+    print("beautifulsoup4 installation test passed..... overall progress 30%\n\n")
 
 except AssertionError:
-    print ('beautifulsoup4 installation test Failed')
+    print ('beautifulsoup4 installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("chardet" in installed_packages)
-    print("chardet installation test passed..... overall progress 40%")
+    print("chardet installation test passed..... overall progress 40%\n\n")
 
 except AssertionError:
-    print ('chardet installation test Failed')
+    print ('chardet installation test Failed\n\n')
+    Fail_Program()
 
 try:
-    print("idna installation test passed..... overall progress 50%")
+    print("idna installation test passed..... overall progress 50%\n\n")
     assert ("importlib-metadata" in installed_packages)
 
 except AssertionError:
-    print ('idna installation test Failed')
+    print ('idna installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("importlib-metadata" in installed_packages)
-    print("importlib-metadata installation test passed..... overall progress 60%")
+    print("importlib-metadata installation test passed..... overall progress 60%\n\n")
 
 except AssertionError:
-    print ('importlib-metadata installation test Failed')
+    print ('importlib-metadata installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("more-itertools" in installed_packages)
-    print("more-itertools installation test passed..... overall progress 70%")
+    print("more-itertools installation test passed..... overall progress 70%\n\n")
 
 except AssertionError:
-    print ('more-itertools installation test Failed')
+    print ('more-itertools installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("packaging" in installed_packages)
-    print("packaging installation test passed..... overall progress 80%")
+    print("packaging installation test passed..... overall progress 80%\n\n")
 
 except AssertionError:
-    print ('packaging installation test Failed')
+    print ('packaging installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("pluggy" in installed_packages)
-    print("pluggy installation test passed..... overall progress 90%")
+    print("pluggy installation test passed..... overall progress 90%\n\n")
+
 except AssertionError:
-    print ('pluggy installation test Failed')
+    print ('pluggy installation test Failed\n\n')
+    Fail_Program()
 
 try:
     assert ("py" in installed_packages)
-    print("py installation test passed..... overall progress 100%")
+    print("py installation test passed..... overall progress 100%\n\n")
 
 except AssertionError:
-    print ('py installation test Failed')
+    print ('py installation test Failed\n\n')
+    Fail_Program()
